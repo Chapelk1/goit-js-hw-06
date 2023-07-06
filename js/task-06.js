12d837ae1d7ba84a3fee0f555c0e1afa0d762a93
Chapelk1 <klepach154789@gmail.com>
2023-07-06 13:32:06 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,12 @@
 const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener('blur', (event) => {
-    const value = event.currentTarget.value.length
-    if (value === 6) {
-        inputEl.classList.remove("invalid");
-        inputEl.classList.add('valid')
-        return
+    const value = event.currentTarget.value.length;
+    const currentNumber = Number(event.currentTarget.dataset.length);
+    if (value === currentNumber) {
+      inputEl.classList.remove("invalid");
+      inputEl.classList.add("valid");
+      return;
     }
         inputEl.classList.add('invalid')
 })
